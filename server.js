5396f343ce710dcce07540fc92220f5f5742d3b5
jabzazad <jab-love@hotmail.co.th>
2018-09-17 10:44:14 +0700

    Go

--- server.js
+++ server.js
@@ -1,16 +1,42 @@
 var express=require('express');
 var app=express();
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'www.db4free.net',
-    user     : 's140390',
-    password : 'abc123**',
-    database : 'db140390'
-  });
-  
-  connection.connect();
+
 app.set('view engine','ejs');
 app.get('/',function(req,res){
 res.render('pages/index');
 });
+
+app.get('/students',function(req,res){
+    var connection = mysql.createConnection({
+        host     : 'www.db4free.net',
+        user     : 's140390',
+        password : 'abc123**',
+        database : 'db140390'
+      });
+    
+    connection.connect()
+    connection.query('SELECT * FROM students', function(err, rows, fields) {  
+        if (err) throw err
+            res.render('pages/students', {students : rows}); 
+            
+
+    })
+    connection.end()
+});
+app.get('/subjects',function(req,res){
+    var connection = mysql.createConnection({
+        host     : 'www.db4free.net',
+        user     : 's140390',
+        password : 'abc123**',
+        database : 'db140390'
+      });
+      connection.connect()
+      connection.query('SELECT * FROM subjects', function(err, rows, fields) {  
+        if (err) throw err
+            res.render('pages/subjects', {subjects : rows}); 
+    })
+    connection.end()
+});
+
 app.listen(4000);
